b64247a157be370eebda94a46f703638adf10e63
Malasia Woods <55638544+malasia-415@users.noreply.github.com>
2020-05-14 21:38:44 -0700

    Create Creating pairs.js

---
+++ Creating pairs.js
@@ -0,0 +1,9 @@
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+The big O for this algorithm is O(log n).
